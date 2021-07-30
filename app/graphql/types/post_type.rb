fac839fae28ad440941709f2c0136cf5006b7f44
TurVitAn <vitalii.turzhanskyi@gmail.com>
2021-07-30 08:21:05 +0300

    Change GraphQL Types for User and Post

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -1,10 +1,7 @@
 module Types
   class PostType < Types::BaseObject
-    field :id, ID, null: false
-    field :user_id, Integer, null: false
-    field :title, String, null: true
-    field :body, String, null: true
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :id, Integer, null: false
+    field :title, String, null: false
+    field :body, String, null: false
   end
 end
